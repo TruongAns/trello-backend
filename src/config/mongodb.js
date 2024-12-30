aac1cddf80b77b33b3eea6642b460c76ded87695
TruongAn <truonganspkt2014@gmail.com>
2024-12-30 12:55:48 +0700

    init MongoDb

--- src/config/mongodb.js
+++ src/config/mongodb.js
@@ -1,3 +1,38 @@
 /**
 
  */
+import { env } from '~/config/environment';
+
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+const url = env.MONGODB_URI;
+const dbName = env.DATABASE_NAME;
+
+let trelloDatabaseInstance = null;
+
+const client = new MongoClient(url, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true
+  }
+});
+
+export const CONNECT_DB = async () => {
+  console.log('Start Connecting to MongoDb...');
+  await client.connect();
+  console.log('Connected Success to MongoDb...');
+  trelloDatabaseInstance = client.db(dbName);
+};
+
+export const GET_DB = () => {
+  if (!trelloDatabaseInstance) throw new Error('Must connect to Database First');
+  return trelloDatabaseInstance;
+};
+
+export const CLOSE_DB = async () => {
+  console.log('Starting to disconnect from the database');
+  await client.close();
+  trelloDatabaseInstance = null;
+  console.log('Database disconnected successfully');
+};
--- src/server.js
+++ src/server.js
@@ -1,30 +1,51 @@
 import express from 'express';
 import { mapOrder } from '~/utils/sorts.js';
 import { env } from '~/config/environment';
+import { CONNECT_DB, GET_DB, CLOSE_DB } from '~/config/mongodb';
+const exitHook = require('async-exit-hook');
+
 const app = express();
 
 const hostname = env.APP_HOST;
 const port = env.APP_PORT;
 
-app.get('/', (req, res) => {
-  // Test Absolute import mapOrder
-  console.log(
-    mapOrder(
-      [
-        { id: 'id-1', name: 'One' },
-        { id: 'id-2', name: 'Two' },
-        { id: 'id-3', name: 'Three' },
-        { id: 'id-4', name: 'Four' },
-        { id: 'id-5', name: 'Five' }
-      ],
-      ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
-      'id'
-    )
-  );
-  res.end('<h1>Hello World!</h1><hr>');
-});
+const START_SERVER = () => {
+  app.get('/', async (req, res) => {
+    // Test Absolute import mapOrder
+    console.log(await GET_DB().listCollections().toArray());
 
-app.listen(port, hostname, () => {
-  // eslint-disable-next-line no-console
-  console.log(`Hello DTA Dev, I am running at http://${hostname}:${port}/`);
-});
+    console.log(
+      mapOrder(
+        [
+          { id: 'id-1', name: 'One' },
+          { id: 'id-2', name: 'Two' },
+          { id: 'id-3', name: 'Three' },
+          { id: 'id-4', name: 'Four' },
+          { id: 'id-5', name: 'Five' }
+        ],
+        ['id-5', 'id-4', 'id-2', 'id-3', 'id-1'],
+        'id'
+      )
+    );
+    res.end('<h1>Hello World!</h1><hr>');
+  });
+
+  app.listen(port, hostname, () => {
+    // eslint-disable-next-line no-console
+    console.log(`Hello DTA Dev, I am running at http://${hostname}:${port}/`);
+  });
+
+  exitHook(() => {
+    CLOSE_DB();
+  });
+};
+
+(async () => {
+  try {
+    await CONNECT_DB();
+    START_SERVER();
+  } catch (error) {
+    console.log(error);
+    process.exit(0);
+  }
+})();
